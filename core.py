b5cac998812a361dd60598eb9fda72bf59fa09fa
PaulSka <lysakowskimg@gmail.com>
2012-03-30 17:59:54 +0200

    update cursegui.py, fix curses crash

--- core.py
+++ core.py
@@ -1,192 +1,225 @@
-"""
-Partie Core
-"""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-##TODO
-#Voir fichier TODO
+#**************************************************
+# SnakePY (Coding party projet)
+#
+#**************************************************
 
+#Importation des modules
 import random
 import cPickle
 import os
 import operator
 import ConfigParser
 
+
 class Game():
-  def __init__(self, config):
-    #On instancie notre classe Config
-    self.config = config #Config()
+    """
+    Classe Game.
+    Contient les éléments nécessaires pour le jeu
+    """
+    def __init__(self, config):
+        """
+        Constructeur par défaut.
+        Cette méthode est automatiquement chargée a l'appel
+        de la classe Game()
+        """
+        #On instancie notre classe Config
+        self.config = config
 
-    #On instancie notre classe HighScore
-    self.highscore = HighScore()
-    
-    #On instancie notre classe Apple
-    x = self.config.rawConfig.getint("GLOBAL", "x")
-    y = self.config.rawConfig.getint("GLOBAL", "y")    
-    self.apple = Apple(x, y)
+        #On instancie notre classe HighScore
+        self.highscore = HighScore()
+
+        #On instancie notre classe Apple
+        x = self.config.rawConfig.getint("GLOBAL", "x")
+        y = self.config.rawConfig.getint("GLOBAL", "y")    
+        self.apple = Apple(x, y)
 
 
-  def join(self):
-    '''
-    Le joueur courant rejoint la partie
-    '''
-    #On instancie notre Snake en lui indiquant sa position par defaut
-    #et sa taille par defaut
-    self.snake = Snake([30,7], 2)
-    
-    #On instancie notre Joueur
-    self.player = Player()    
-    self.player.setName(self.config.rawConfig.get("PLAYER", "name"))
-    
+    def join(self):
+        '''
+        Le joueur courant rejoint la partie
+        '''
+        #On instancie notre Snake en lui indiquant sa position par defaut
+        #et sa taille par defaut
+        self.snake = Snake([30,7], 2)
+
+        #On instancie notre Joueur
+        self.player = Player()    
+        self.player.setName(self.config.rawConfig.get("PLAYER", "name"))
+
   
 class Player():
-	"""
-	Creation de la classe Player
-	"""
+    """
+    Creation de la classe Player
+    """
 
-	score = 0
+    #On initialise son score a 0
+    score = 0
 
-	def __init__(self, name="Snake"):
-		"""
-		Methode execute a l'instanciation de la classe Player
-		On fixe le nom du joueur a Snake par defaut
-		"""
-		self.setName(name)
+    def __init__(self, name="Snake"):
+        """
+        Methode execute a l'instanciation de la classe Player
+        On fixe le nom du joueur a Snake par defaut
+        """
 
-	def setName(self, name):
-		"""
-		Methode permettant de fixer le nom du joueur
-		"""
-		self.name = name
+        #On lui affecte son nom
+        self.setName(name)
 
-	def addPoint(self):
-		"""
-		Methode permettant d'ajouter 1 point au score du joueur
-		"""
-		self.score +=1
+    def setName(self, name):
+        """
+        Methode permettant de fixer le nom du joueur
+        """
+        #On affecte le nom
+        self.name = name
+
+    def addPoint(self):
+        """
+        Methode permettant d'ajouter 1 point au score du joueur
+        """
+        #On ajoutte  +1 au score courant
+        self.score +=1
 
 class HighScore():
-	"""
-	Creation de la classe HighScore
-	"""
-	def __init__(self):
-		"""
-		Methode execute a l'instanciation de la classe HighScore
-		"""
-		#On charge notre fichier pickle si il existe,
-		#sinon on le creer
-		self.oFullPath = 'highspkl'
-		if os.path.isfile(self.oFullPath):
-			self.highScore = cPickle.load(open(self.oFullPath, 'rb'))
-		else:
-			self.highScore = {}
+    """
+    Creation de la classe HighScore
+    """
+    def __init__(self):
+        """
+        Methode execute a l'instanciation de la classe HighScore
+        """
+        #On charge notre fichier pickle si il existe,
+        #sinon on le creer
+        self.oFullPath = 'highspkl'
+        if os.path.isfile(self.oFullPath):
+            self.highScore = cPickle.load(open(self.oFullPath, 'rb'))
+        else:
+            self.highScore = {}
 
-	def addHighScore(self, player, score):
-		"""
-		methode permettant d'ajouter un resultat au HighScore
-		"""
-		self.highScore[player] = score
+    def addHighScore(self, player, score):
+        """
+        Methode permettant d'ajouter un resultat au HighScore
+        """
+        #On ajoutte le score du joueur au highscore
+        self.highScore[player] = score
 
-	def writeHighScore(self):
-		"""
-		Methode permettant de sauvegarder les HighScores
-		Structure du HighScore
-		{
-			'John': 16,
-			'Bryan': 1,
-			'Joe': 9,
-			...
-			'Snake': 8,
-		}
-		"""
-		if isinstance(self.highScore, list):
-			self.highScore = dict(self.highScore)
-		cPickle.dump(self.highScore, open(self.oFullPath, 'wb'))
-		
-	def showHighScore(self):
-		"""
-		Methode permettant d'afficher les meilleurs HighScores
-		"""
-		#On doit tester si la liste de scores est vide (initialisation)
-		#ou contient deja des valeurs
-		#
-		
-		return sorted(self.highScore.iteritems(), key=operator.itemgetter(1), reverse=True)
+    def writeHighScore(self):
+        """
+        Methode permettant de sauvegarder les HighScores
+        Structure du HighScore
+        {
+            'John': 16,
+            'Bryan': 1,
+            'Joe': 9,
+            ...
+            'Snake': 8,
+        }
+        """
+        if isinstance(self.highScore, list):
+            self.highScore = dict(self.highScore)
+        #On écrit le highscore dans le fichier Pickle
+        #via la méthode dump()
+        cPickle.dump(self.highScore, open(self.oFullPath, 'wb'))
+
+    def showHighScore(self):
+        """
+        Methode permettant d'afficher les meilleurs HighScores
+        """
+        #On renvoie la liste des highscore dans le sens inverse.
+        return sorted(self.highScore.iteritems(), key=operator.itemgetter(1), reverse=True)
 
 class Snake():
-	"""
-	Creation de la classe Snake
-	"""
+    """
+    Creation de la classe Snake
+    """
 
-	def __init__(self, initPos, lSnake=2):
-		"""
-		Methode execute a l'instanciation de la classe Snake
-		"""
-		self.oSnake = self.createSnake(initPos, lSnake)
-	
-	def createSnake(self, initPos, lSnake):
-		"""
-		Methode permettant de creer le Snake
-		initPos est un couple x,y
-		lSnake represente la longueur par defaut (2) 
-		"""
+    def __init__(self, initPos, lSnake=2):
+        """
+        Methode execute a l'instanciation de la classe Snake
+        """
+        #On créer notre serpent
+        self.oSnake = self.createSnake(initPos, lSnake)
 
-		#On creer notre Snake sous la forme de coordonnees
-		#[x,y]
-		snake = []
-		#Pour la longueur souhaite, on fait 'grandir'' notre
-		#Snake
-		for i in range(lSnake):
-			snake.append([initPos[0], initPos[1]+i])
-		
-		#On renvoie les coordonnes du Snake
-		return snake
+    def createSnake(self, initPos, lSnake):
+        """
+        Methode permettant de creer le Snake
+        initPos est un couple x,y
+        lSnake represente la longueur par defaut (2) 
+        """
 
-	def move(self, key):
-		"""
-		Methode pour le mouvement du snake
-		"""
-		if key == 260:
-			self.oSnake.insert(0, [self.oSnake[0][0] -1, self.oSnake[0][1]])
-		elif key == 261:
-			self.oSnake.insert(0, [self.oSnake[0][0] +1, self.oSnake[0][1]])
-		elif key == 259:
-			self.oSnake.insert(0, [self.oSnake[0][0], self.oSnake[0][1] -1])
-		elif key == 258:
-			self.oSnake.insert(0, [self.oSnake[0][0], self.oSnake[0][1] +1])
+        #On creer notre Snake sous la forme de coordonnees
+        #[x,y]
+        #Par défaut, il n'y a pas de coordonées
+        snake = []
+        #Pour la longueur souhaite, on fait 'grandir'' notre
+        #Snake
+        for i in range(lSnake):
+            snake.append([initPos[0], initPos[1]+i])
+
+        #On renvoie les coordonnées du Snake
+        return snake
+
+    def move(self, key):
+        """
+        Methode pour le mouvement du snake
+        """
+        #Si la fleche gauche est appuyée....
+        if key == 260:
+            self.oSnake.insert(0, [self.oSnake[0][0] -1, self.oSnake[0][1]])
+        #Si la fleche droite est appuyée...
+        elif key == 261:
+            self.oSnake.insert(0, [self.oSnake[0][0] +1, self.oSnake[0][1]])
+        elif key == 259:
+        #Si la fleche haut est appuyée...
+            self.oSnake.insert(0, [self.oSnake[0][0], self.oSnake[0][1] -1])
+        elif key == 258:
+        #Sil la fleche bas est appuyée...
+            self.oSnake.insert(0, [self.oSnake[0][0], self.oSnake[0][1] +1])
 
 class Apple():
-	"""
-	Classe concernant la pomme
-	"""
+    """
+    Classe concernant la pomme
+    """
 
-	def __init__(self, maxX, maxY):
-		self.maxX = maxX
-		self.maxY = maxY
-		self.newApple()
+    def __init__(self, maxX, maxY):
+        #On définie les coordonnées max (x,y)
+        #La pomme ne doit pas être au dessus des valeurs max.
+        self.maxX = maxX
+        self.maxY = maxY
+        #On genere notre pomme
+        self.newApple()
 
-	def newApple(self):
-		"""
-		Methode permettant de creer les coordonnes d'une pomme
-		La pomme ne doit pas etre creer aux coordonnes du Snake
-		"""
-		self.coordx = random.randrange(1, self.maxX-1)
-		self.coordy = random.randrange(1, self.maxY-1)
+    def newApple(self):
+        """
+        Methode permettant de creer les coordonnes d'une pomme
+        La pomme ne doit pas etre creer aux coordonnes du Snake
+        """
+        #On utilise la fonction random.randrange() pour générer un
+        #nombre aléatoire allant de 1 a max-1
+        self.coordx = random.randrange(1, self.maxX-1)
+        self.coordy = random.randrange(1, self.maxY-1)
 
-	def checkApple(self, snakePos):
-		"""
-		Methode verifiant si les nouvelles coordonnees de la pomme
-		ne sont pas sur les coordonnees du Snake
-		"""
-		if [self.coordx, self.coordy] in snakePos:
-			return False
-		elif [self.coordx, self.coordy] not in snakePos:
-			return True
+    def checkApple(self, snakePos):
+        """
+        Methode verifiant si les nouvelles coordonnees de la pomme
+        ne sont pas sur les coordonnees du Snake
+        """
+        #Si les coordonnées générées sont sur la position du snake...
+        if [self.coordx, self.coordy] in snakePos:
+        #... alors on renvoie False
+            return False
+        elif [self.coordx, self.coordy] not in snakePos:
+        #... si ce n'est pas le cas, on revoie True
+            return True
 
 class Config():
-	"""
-	Classe concernant la configuration
-	"""
+    """
+    Classe concernant la configuration
+    """
 
-	def __init__(self):
-		self.rawConfig = ConfigParser.ConfigParser()
-		self.rawConfig.read('conf.ini')
+    def __init__(self):
+        #On instancie le module ConfigParser()
+        self.rawConfig = ConfigParser.ConfigParser()
+        #On charge le fichier de conf
+        self.rawConfig.read('conf.ini')
--- cursegui.py
+++ cursegui.py
@@ -1,31 +1,109 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+#**************************************************
+# SnakePY (Coding party projet)
+#**************************************************
 
-"""
-Partie GUI pour le snake
-"""
-
-##TODO
-#Voir fichier TODO
-
+#Importation des modules
 import curses
 import core
+from optparse import OptionParser
+import sys
 
-def clearScreen():
+#GLOBAL
+global config, game
+
+def initCurse():
+    #On initialise notre fenêtre
+    curses.initscr()
+    curses.curs_set(0)
+
+def closeCurse():
+    #On détruit les fenêtres curses
+    curses.endwin()
+    #On sort du script
+    exit()
+
+def createNewWin(curses):
     """
-    Efface l'ecran et redessine les bordures
+    On créer une nouvelle fenetre Curse
     """
-    win.clear()
-    #On dessine les contours
+
+    #On dimensionne notre fenetre
+    x = config.rawConfig.getint("GLOBAL", "x")
+    y = config.rawConfig.getint("GLOBAL", "y")
+
+    #On créer la fenêtre
+    win = curses.newwin(x,y,0,0)
+
+    #On affecte les différents paramètres a notre fenêtre
+    win.keypad(1)
+    win.nodelay(1)
     win.border(0)
 
+    return win
 
+def destroyWin():
+    """
+    Fonction permettant de détruire les
+    fenêtres curses
+    """
+    curses.endwin()
+
+def menu():
+
+    #On créer une nouvelle fenetre
+    win = createNewWin(curses)
+
+    chooseMenu = 0
+    
+    #On ajoute les entrées
+    win.addstr(4, 4, 'SnakePY')
+    win.addstr(5, 4, 'Choose option')
+    win.addstr(6, 4, '1. Launch game')
+    win.addstr(7, 4, '2. Show HighScore')
+    win.addstr(8, 4, '3. Credits')
+    win.addstr(9, 4, '4. Exit game')
+    win.addstr(10, 4, '')
+
+    while chooseMenu != ord('4'):
+        #On récupere la touche appuyée par l'utilisateur
+        chooseMenu = win.getch()
+        #Si il s'agit du 1 ...
+        if chooseMenu == ord('1'):
+            #...on lance le jeu
+            destroyWin()
+            launchGame()
+        #Si il s'agit du 2 ...
+        elif chooseMenu == ord('2'):
+            #...on affiche le highscore
+            destroyWin()
+            showHG()
+        #Si il s'agit du 3 ...
+        elif chooseMenu == ord('3'):
+            #... on affiche les crédits
+            destroyWin()
+            showCredits()
+    #Si on sort de la boucle (4), alors on
+    #détruit les fenetres
+    destroyWin()
+
+    #On ferme toutes les fenêtres de curses
+    closeCurse()
+    
 def launchGame():
-
-    clearScreen()
-
+    """
+    Génére une nouvelle fenêtre curse et lance le jeu
+    """    
     # On rejoint la partie
     game.join()
+
+    #On affecte le nom
+    game.player.setName(options.name)
+
+    #On créer une nouvelle fenetre
+    win = createNewWin(curses)
 
     #On creer notre premiere pomme...
     win.addch(game.apple.coordx, game.apple.coordy, 'O')
@@ -34,146 +112,193 @@ def launchGame():
     key = curses.KEY_RIGHT
 
     #On effectue une boucle infinie tant que la touche Echap (27) n'est pas
-    #presse
+    #pressée.
     while key != 27:
-	    #On ajoutte le score a la ligne 0, colonne 2
-	    #Le score est calcule en recuperant la longueur du serpent actuel
-	    #et en retirant 2 (sa valeur initiale)	
-	    win.addstr(0,2,' Joueur : %s Score : %s ' %(game.player.name, str(game.player.score)))
+        #On ajoutte le score a la ligne 0, colonne 2
+        #Le score est calcule en recuperant la longueur du serpent actuel
+        #et en retirant 2 (sa valeur initiale)	
+        win.addstr(0,2,' Joueur : %s Score : %s ' %(game.player.name, str(game.player.score)))
 
-	    #On calcul un mouvement de ralentissement dependant de la longueur du
-	    #serpent
-	    win.timeout(180+ ( (len(game.snake.oSnake)-2) % 10- (len(game.snake.oSnake)-2) ) * 3 )
+        #On calcul un mouvement de ralentissement dependant de la longueur du
+        #serpent
+        win.timeout(180+ ( (len(game.snake.oSnake)-2) % 10- (len(game.snake.oSnake)-2) ) * 3 )
 
-	    #On 'hook' les touches
-	    getkey = win.getch()
+        #On 'hook' les touches
+        getkey = win.getch()
 
-	    #On recupere la valeur de la touche par defaut
-	    key = key if getkey==-1 else getkey
+        #On recupere la valeur de la touche par defaut
+        key = key if getkey==-1 else getkey
 
-	    #Suivant la touche presse, on modifie les positions de notre serpent
-	    game.snake.move(key)
+        #Suivant la touche pressée, on modifie les positions de notre serpent
+        game.snake.move(key)
 
-	    #On supprime les derniers elements sur lequel le Snake passe
-	    win.addch(game.snake.oSnake[len(game.snake.oSnake)-1][1],
-	        game.snake.oSnake[len(game.snake.oSnake)-1][0],' ')
+        #On supprime les derniers elements sur lequel le Snake passe
+        win.addch(game.snake.oSnake[len(game.snake.oSnake)-1][1],
+            game.snake.oSnake[len(game.snake.oSnake)-1][0],' ')
 
-	    #On supprime un element du snake pour eviter la collision
-	    if win.inch(game.snake.oSnake[0][1],
-	            game.snake.oSnake[0][0]) & 255 == 32:
-      		game.snake.oSnake.pop()
+        #On supprime un element du snake pour eviter la collision
+        if win.inch(game.snake.oSnake[0][1], game.snake.oSnake[0][0]) & 255 == 32:
+            game.snake.oSnake.pop()
 
-	    #Si on passe sur un element O	
-	    elif win.inch(game.snake.oSnake[0][1],
-	          game.snake.oSnake[0][0]) & 255 == ord('O'):
-		    #On ajoutte 1 point a notre Joueur
-		    game.player.addPoint()
-	
-		    #On recalcule des nouvelles coordonnees pour la pomme
-		    game.apple.newApple()
-		    #On verifie les nouvelles coordonnees
-		    while game.apple.checkApple(game.snake.oSnake) != True:
-			    game.apple.newApple()
+        #Si on passe sur un element O	
+        elif win.inch(game.snake.oSnake[0][1],game.snake.oSnake[0][0]) & 255 == ord('O'):
+            #On ajoutte 1 point a notre Joueur
+            game.player.addPoint()
 
-		    #On l'affiche a l'ecran
-		    win.addch(game.apple.coordx, game.apple.coordy, 'O')
+            #On recalcule des nouvelles coordonnees pour la pomme
+            game.apple.newApple()
+            #On verifie les nouvelles coordonnees
+            while game.apple.checkApple(game.snake.oSnake) != True:
+                game.apple.newApple()
+
+            #On l'affiche a l'ecran
+            win.addch(game.apple.coordx, game.apple.coordy, 'O')
 		
-	    else:
-	      break
+        else:
+            break
 
-	    #On affiche une partie de notre Snake
-	    win.addch(game.snake.oSnake[0][1],game.snake.oSnake[0][0],'X')
+        #On affiche une partie de notre Snake
+        win.addch(game.snake.oSnake[0][1],game.snake.oSnake[0][0],'X')
 
-    #On met a jour le HighScore et on sauvegarde.
+
+    #Si on sort de la boucle (GameOver), alors on
+    #détruit les fenetres
+    destroyWin()
+
+    #A la fin de la partie (game over), on affiche l'écran    
+    showGameOver()
+
+def showGameOver():
+    """
+    Affiche l'écran GameOver
+    """
+
+    #On créer une nouvelle fenetre
+    win = createNewWin(curses)
+
+    win.addstr(4, 4, 'GAME OVER')
+    win.addstr(5, 4, 'Your Score')    
+    win.addstr(6, 4, '%s - %s' %(game.player.name, game.player.score))
+    win.addstr(8, 4, 'Press 1 to return previous menu')
+    win.addstr(9, 4, '')
+
+    #Ajout dans le highscore
     game.highscore.addHighScore(game.player.name, game.player.score)
     game.highscore.writeHighScore()
 
-    clearScreen()
+    key = 0
+    #Tant que la touche 1 n'est pas pressée...
+    #while key!= 343 or key!=10:
+    while key != ord('1'):
+        #On attend et on 'hook' les touches
+        key = win.getch()
+
+    #Si on sort de la boucle (1), alors on
+    #détruit les fenetres
+    destroyWin()
+
+    #A la fin de la partie (game over), on affiche l'écran    
+    menu()
 
 def showHG():
-    clearScreen()    
+    """
+    Affiche l'écran de highscore
+    """
+
+    #On créer une nouvelle fenetre
+    win = createNewWin(curses)
+    
     win.addstr(4, 4, 'SnakePY HighScore')
     win.addstr(5, 4, 'Press 1 to return previous menu')
+    win.addstr(6, 4, '')
+
+
     #On boucle sur les HighScore
-    x = 7
+    i = 7
+    #Pour chaque entrée dans le highscore...
     for hg in game.highscore.showHighScore():
-        win.addstr(x, 4, "%s -- %s" %(hg[0], hg[1]))
-        x+=1
-    stop = 0
-    while stop!= ord('1'):
-        stop = win.getch()
+        #On ajoutte une ligne
+        win.addstr(i, 4, "%s -- %s" %(hg[0], hg[1]))
+        i+=1
+
+    chooseMenu = 0
+    #Tant que la touche 1 n'est pas pressée...
+    while chooseMenu!= ord('1'):
+        #On attend et on 'hook' les touches
+        chooseMenu = win.getch()
+
+    #Si on sort de la boucle (4), alors on
+    #détruit les fenetres
+    destroyWin()
+
+    #...sinon on sort de la boucle et on affiche de
+    #de nouveau le menu    
     menu()
-    return
 
 def showCredits():
-    import time
-    clearScreen()    
-    win.addstr(4, 4, 'SnakePY Credits')
+    """
+    Affiche les crédits via le fichier CREDITS
+    """
+
+    #On créer une nouvelle fenetre
+    win = createNewWin(curses)
+
+    win.addstr(4, 4, 'CREDITS')
     win.addstr(5, 4, 'Press 1 to return previous menu')
-    x = 7
+    win.addstr(6, 4, '')
+
     #On charge le fichier credits
     f = open('CREDITS', 'rb')
     #On recupere la ligne
     lines = f.readlines()    
-    #On affiche les lignes
+    #Pour chaque ligne présentes....
+    i = 7
     for line in lines:
-        win.addstr(x, 4, line.strip())
-        x+=1
-    stop = 0
-    while stop!= ord('1'):
-        stop = win.getch()
+        #...on ajoutte les infos en supprimant les 'blancs' (strip())
+        win.addstr(i, 4, line.strip())
+        i+=1
+    chooseMenu = 0
+    #Tant que la touche 1 n'est pas pressée...
+    while chooseMenu!= ord('1'):
+        #On attend et on 'hook' les touches
+        chooseMenu = win.getch()
+
+    #Si on sort de la boucle (1), alors on
+    #détruit les fenetres
+    destroyWin()
+
+    #...sinon on sort de la boucle et on affiche de
+    #de nouveau le menu    
     menu()
-    return
-    
-    
-def menu():
-    #Menu
-    x = 0
 
-    clearScreen()
+#-------------------------
+#Initialisation du jeu 
+#-------------------------
 
-    #On boucle pour le menu    
-    while x!= ord('4'):
-        #On ajoute les entrees
-        win.addstr(4, 4, 'SnakePY')
-        win.addstr(5, 4, 'Choose option')
-        win.addstr(6, 4, '1. Launch game')
-        win.addstr(7, 4, '2. Show HighScore')
-        win.addstr(8, 4, '3. Credits')
-        win.addstr(9, 4, '4. Exit game')
-        #On recupere la touche appuye par l'utilisateur
-        x = win.getch()
-        if x == ord('1'):
-            #Si il s'agit du 1 ...
-            launchGame()
-        if x == ord('2'):
-            showHG()
-        if x == ord('3'):
-            showCredits()
-    return    
-
-#On initialise notre fenetre
-curses.initscr()
-curses.curs_set(0)
+#Partie OptParse
+parser = OptionParser()
+parser.add_option('-n', '--name', dest="name", help="Name", default="Snake", type="string")
+(options, args) = parser.parse_args()
 
 #On instancie notre classe Config
 config = core.Config()
 
-#On dimensionne notre fenetre
-x = config.rawConfig.getint("GLOBAL", "x")
-y = config.rawConfig.getint("GLOBAL", "y")
-win = curses.newwin(x,y,0,0)
 
-#?
-win.keypad(1)
-win.nodelay(1)
+try:
+    #On initialise Curses()
+    initCurse()
 
-#On instancie notre partie
-game = core.Game(config)
+    #On dimensionne notre fenetre
+    x = config.rawConfig.getint("GLOBAL", "x")
+    y = config.rawConfig.getint("GLOBAL", "y")
 
-      
-#On affiche le menu
-menu()
+    #On instancie notre partie
+    game = core.Game(config)
 
-curses.endwin()
-exit()
+    #On affiche le menu
+    menu()
+
+except:
+    closeCurse()
+    sys.exit("Error")
+
